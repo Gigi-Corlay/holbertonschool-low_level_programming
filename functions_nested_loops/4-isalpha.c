c6f10635e71acef1a0f6c3ccc28813ec30668bef
Gigi-Corlay <11489@holbertonstudents.com>
2025-10-22 12:22:24 +0000

    Write a function that checks for alphabetic character.

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * _isalpha - checks if the character is an alphabetic letter.
  * @c: the character to be checked.
  *
- * Return: 1 if c is an alphabetic letter (either 
+ * Return: 1 if c is an alphabetic letter (either
  *         lowercase 'a' to 'z' or uppercase 'A' to 'Z'),
  *         0 otherwise.
  */
