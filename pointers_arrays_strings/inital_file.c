f472bff91fd341214578c0ff2a860cae44e124b4
Gigi-Corlay <11489@holbertonstudents.com>
2025-11-02 20:49:06 +0100

    Add one line to this code, so that the program prints a[2] = 98, followed by a new line.

--- pointers_arrays_strings/inital_file.c
+++ pointers_arrays_strings/inital_file.c
@@ -1,12 +1,5 @@
 #include <stdio.h>
 
-/**
-* main - Entry point
-*
-* Description: Modify a[2] to 98 using the pointer p
-*
-* Return: Always 0
-*/
 int main(void)
 {
 	int n;
@@ -15,7 +8,14 @@ int main(void)
 
 	a[2] = 1024;
 	p = &a[2];
-
+	/*
+	* write your line of code here...
+	* Remember:
+	* - you are not allowed to use a
+	* - you are not allowed to modify p
+	* - only one statement
+	* - you are not allowed to code anything else than this line of code
+	*/
 	*p = 98;
 
 	if (a[2] == 98)
