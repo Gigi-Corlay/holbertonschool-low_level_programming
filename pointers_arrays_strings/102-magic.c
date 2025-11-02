72e4e077e01c7e940dcc2c854c57ab88f3257662
Gigi-Corlay <11489@holbertonstudents.com>
2025-11-02 20:28:45 +0100

    Add one line to this code, so that the program prints a[2] = 98, followed by a new line.

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
 * main - Entry point
 * Description: Adds one line to modify a[2] to 98 using the pointer p
 *              and prints the value to check correctness.
-*
 * Return: Always 0.
 */
 int main(void)
