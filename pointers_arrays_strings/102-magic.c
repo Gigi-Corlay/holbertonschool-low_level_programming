187c8c8cb1c74bc3e145f5994d8d9df0c147a1a7
Gigi-Corlay <11489@holbertonstudents.com>
2025-11-02 20:00:56 +0100

    Add one line to this code, so that the program prints a[2] = 98, followed by a new line.

---
+++ pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: Adds one line to modify a[2] to 98 using the pointer p
+*              and prints the value to check correctness.
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int a[5];
+	int *p = &a[2];
+	a[2] = 1024;
+
+	if (a[2] == 98)
+	{
+		printf("a[2] = %d\n", a[2]);
+	}
+
+	return (0);
+}
