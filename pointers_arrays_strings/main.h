dd468f6088f8c6faf6e1112c88f4bc3c6e241052
Gigi-Corlay <11489@holbertonstudents.com>
2025-11-03 11:15:40 +0100

    Write a function that copies memory area.

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,5 +25,6 @@ char *leet(char *s);
 void print_number(int n);
 char *rot13(char *s);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif
