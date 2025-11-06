38801164e16756468857c11bb35b8b4289ba7adf
Gigi-Corlay <11489@holbertonstudents.com>
2025-11-06 15:36:02 +0100

    Write a function that returns 1 if a string is a palindrome and 0 if not.

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -3,15 +3,26 @@
 #include <string.h>
 
 /**
- * helper_palindrome - Vérifie récursivement si une sous-chaîne est un palindrome
- * @s: chaîne de caractères à vérifier
- * @start: index du début de la sous-chaîne
- * @end: index de la fin de la sous-chaîne
- *
- * Return: 1 si la sous-chaîne est un palindrome, 0 sinon
- */
+* helper_palindrome - Vérifie récursivement si une sous-chaîne est un palindrome
+* en ignorant les espaces
+* @s: chaîne à vérifier
+* @start: index de début
+* @end: index de fin
+*
+* Return: 1 si palindrome, 0 sinon
+*/
 int helper_palindrome(char *s, int start, int end)
 {
+	while (start < end && s[start] == ' ')
+	{
+		start++;
+	}
+
+	while (start < end && s[end] == ' ')
+	{
+		end--;
+	}
+
 	if (start >= end)
 	{
 		return (1);
@@ -24,11 +35,11 @@ int helper_palindrome(char *s, int start, int end)
 }
 
 /**
- * is_palindrome - Vérifie si une chaîne est un palindrome
- * @s: chaîne de caractères à vérifier
- *
- * Return: 1 si la chaîne est un palindrome, 0 sinon
- */
+* is_palindrome - Vérifie si une chaîne est un palindrome
+* @s: chaîne de caractères à vérifier
+*
+* Return: 1 si la chaîne est un palindrome, 0 sinon
+*/
 int is_palindrome(char *s)
 {
 	int len;
