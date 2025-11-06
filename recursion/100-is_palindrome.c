88d53e13a9226fd3dd6398e1821e17e0dcf7bc99
Gigi-Corlay <11489@holbertonstudents.com>
2025-11-06 15:42:57 +0100

    Write a function that returns 1 if a string is a palindrome and 0 if not.

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -1,59 +1,62 @@
 #include "main.h"
-#include <stdio.h>
+#include <ctype.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* helper_palindrome - Vérifie récursivement si une sous-chaîne est un palindrome
-* en ignorant les espaces
-* @s: chaîne à vérifier
-* @start: index de début
-* @end: index de fin
-*
-* Return: 1 si palindrome, 0 sinon
-*/
-int helper_palindrome(char *s, int start, int end)
+ * _strlen_recursion - Retourne la longueur d'une chaîne
+ * @s: chaîne de caractères
+ *
+ * Return: longueur de la chaîne
+ */
+int _strlen_recursion(char *s)
 {
-	while (start < end && s[start] == ' ')
-	{
-		start++;
-	}
-
-	while (start < end && s[end] == ' ')
-	{
-		end--;
-	}
-
-	if (start >= end)
-	{
-		return (1);
-	}
-	if (s[start] != s[end])
-	{
+	if (*s == '\0')
 		return (0);
-	}
-	return (helper_palindrome(s,start + 1, end - 1));
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
-* is_palindrome - Vérifie si une chaîne est un palindrome
-* @s: chaîne de caractères à vérifier
-*
-* Return: 1 si la chaîne est un palindrome, 0 sinon
-*/
+ * helper_palindrome - Vérifie récursivement si une sous-chaîne est un palindrome
+ * en ignorant les espaces et la casse
+ * @s: chaîne à vérifier
+ * @start: index de début
+ * @end: index de fin
+ *
+ * Return: 1 si palindrome, 0 sinon
+ */
+int helper_palindrome(char *s, int start, int end)
+{
+	while (start < end && s[start] == ' ')
+		start++;
+	while (start < end && s[end] == ' ')
+		end--;
+
+	if (start >= end)
+		return (1);
+
+	if (tolower(s[start]) != tolower(s[end]))
+		return (0);
+
+	return (helper_palindrome(s, start + 1, end - 1));
+}
+
+/**
+ * is_palindrome - Vérifie si une chaîne est un palindrome
+ * @s: chaîne de caractères à vérifier
+ *
+ * Return: 1 si la chaîne est un palindrome, 0 sinon
+ */
 int is_palindrome(char *s)
 {
 	int len;
 
 	if (s == NULL)
-	{
 		return (0);
-	}
 	if (*s == '\0')
-	{
 		return (1);
-	}
 
-	len = strlen(s);
-
+	len = _strlen_recursion(s);
 	return (helper_palindrome(s, 0, len - 1));
 }
