4cf461722af6c1f32e7af751960eb5a9e6cb7608
Gigi-Corlay <11489@holbertonstudents.com>
2025-11-12 16:21:30 +0100

    Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,51 +1,53 @@
-#include <stdio.h>
-#include "dog.h"
 #include <stdlib.h>
 #include <string.h>
+#include "dog.h"
 
 /**
-* new_dog - Creates a new dog
-* @name: Name of the dog
-* @age: Age of the dog
-* @owner: Owner of the dog
-*
-* Return: Pointer to the new dog, or NULL if memory allocation fails
-*/
+ * new_dog - Creates a new dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Return: Pointer to the new dog, or NULL if memory allocation fails
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	char *name_copy;
-	char *owner_copy;
+    dog_t *dog;
+    char *name_copy;
+    char *owner_copy;
 
-	if (name == NULL)
-        name = "";
-    if (owner == NULL)
-        owner = "";
+    /* Si name ou owner est NULL, retourner NULL */
+    if (name == NULL || owner == NULL)
+        return (NULL);
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
+    /* Allouer la structure */
+    dog = malloc(sizeof(dog_t));
+    if (dog == NULL)
+        return (NULL);
 
-	name_copy = malloc(strlen(name) + 1);
-	if (name_copy == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-	strcpy(name_copy, name);
+    /* Allouer et copier name */
+    name_copy = malloc(strlen(name) + 1);
+    if (name_copy == NULL)
+    {
+        free(dog);
+        return (NULL);
+    }
+    strcpy(name_copy, name);
 
-	owner_copy = malloc(strlen(owner) + 1);
-	if (owner_copy == NULL)
-	{
-		free(name_copy);
-		free(dog);
-		return (NULL);
-	}
-	strcpy(owner_copy, owner);
+    /* Allouer et copier owner */
+    owner_copy = malloc(strlen(owner) + 1);
+    if (owner_copy == NULL)
+    {
+        free(name_copy);
+        free(dog);
+        return (NULL);
+    }
+    strcpy(owner_copy, owner);
 
-	dog->name = name_copy;
-	dog->owner = owner_copy;
-	dog->age = age;
+    /* Remplir la structure */
+    dog->name = name_copy;
+    dog->owner = owner_copy;
+    dog->age = age;
 
-	return (dog);
+    return (dog);
 }
