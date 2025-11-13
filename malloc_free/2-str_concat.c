8fee05685adf882ab09d76cbd15d84b1e44531cd
Gigi-Corlay <11489@holbertonstudents.com>
2025-11-13 15:34:33 +0100

    Write a function that concatenates two strings.

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,55 +3,39 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatène deux chaînes de caractères
- * @s1: première chaîne
- * @s2: deuxième chaîne
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
  *
- * Return: pointeur vers la nouvelle chaîne allouée (ou NULL si échec)
+ * Return: pointer to newly allocated concatenated string, or NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = 0;
-	int len2 = 0;
-	int total_len;
-	char *new_str;
-	int i = 0;
-	int j = 0;
+	int i, j, len1 = 0, len2 = 0;
+	char *strTemp;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
+
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
 
-	total_len = len1 + len2 + 1;
-
-	new_str = malloc(sizeof(*new_str) * total_len);
-
-	if (new_str == NULL)
+	strTemp = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (strTemp == NULL)
 		return (NULL);
 
-	while (s1[i] != '\0')
-	{
-		new_str[i] = s1[i];
-		i++;
-	}
-	while (s2[j] != '\0')
-	{
-    	new_str[i] = s2[j];
-    	i++;
-    	j++;
-	}
-	new_str[i] = '\0';	
+	for (i = 0; i < len1; i++)
+		strTemp[i] = s1[i];
 
-	return (new_str);
+	for (j = 0; j < len2; j++)
+		strTemp[i + j] = s2[j];
+
+	strTemp[i + j] = '\0';
+
+	return (strTemp);
 }
