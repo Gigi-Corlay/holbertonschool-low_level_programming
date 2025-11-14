762e02d80456746f8ce96a8037c300c1cbd5e1a4
Gigi-Corlay <11489@holbertonstudents.com>
2025-11-14 13:56:29 +0100

    Write a function that splits a string into words.

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -8,62 +8,64 @@
 */
 char **strtow(char *str)
 {
-	unsigned int i = 0, j, k, w = 0, n = 0, start, end, length;
-	char **words;
+    unsigned int i = 0, j, k, w = 0, n = 0, start, end, length;
+    char **words;
 
-	if (str == NULL || str[0] == '\0')
-		return (NULL);
+    if (str == NULL || str[0] == '\0')
+        return (NULL);
 
-	while (str[i])
-	{
-		while (str[i] == ' ')
-			i++;
-		if (str[i])
-		{
-			n++;
-			while (str[i] != ' ' && str[i] != '\0')
-				i++;
-		}
-	}
+    /* Count words */
+    while (str[i])
+    {
+        while (str[i] == ' ')
+            i++;
+        if (str[i])
+        {
+            n++;
+            while (str[i] != ' ' && str[i] != '\0')
+                i++;
+        }
+    }
 
-	if (n == 0)
-		return (NULL);
+    if (n == 0)
+        return (NULL);
 
-	words = malloc(sizeof(char *) * (n + 1));
-	if (words == NULL)
-		return (NULL);
-	words[n] = NULL;
+    /* Allocate array of pointers */
+    words = malloc(sizeof(char *) * (n + 1));
+    if (words == NULL)
+        return (NULL);
+    words[n] = NULL;
 
-	i = 0;
-	w = 0;
-	while (str[i] && w < n)
-	{
-		while (str[i] == ' ')
-			i++;
-		start = i;
-		while (str[i] != ' ' && str[i] != '\0')
-			i++;
-		end = i;
+    /* Copy words */
+    i = 0;
+    w = 0;
+    while (str[i] && w < n)
+    {
+        while (str[i] == ' ')
+            i++;
+        start = i;
+        while (str[i] != ' ' && str[i] != '\0')
+            i++;
+        end = i;
 
-		length = end - start;
-		words[w] = malloc(sizeof(char) * (length + 1));
-		if (words[w] == NULL)
-		{
-			while (w > 0)
-			{
-				w--;
-				free(words[w]);
-			}
-			free(words);
-			return (NULL);
-		}
+        length = end - start;
+        words[w] = malloc(sizeof(char) * (length + 1));
+        if (words[w] == NULL)
+        {
+            while (w > 0)
+            {
+                w--;
+                free(words[w]);
+            }
+            free(words);
+            return (NULL);
+        }
 
 		for (j = 0, k = start; j < length; j++, k++)
 			words[w][j] = str[k];
-		words[w][length] = '\0';
-		unsigned int placeholder = w++;
+        words[w][length] = '\0';
+		w++;
 		i = end;
 	}
-
 	return (words);
 }
