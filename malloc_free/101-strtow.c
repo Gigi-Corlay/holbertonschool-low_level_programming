ade88e0a9a5ac52bbd62275b92bd01f59cdc0111
Gigi-Corlay <11489@holbertonstudents.com>
2025-11-14 12:35:09 +0100

    Write a function that splits a string into words.

---
+++ malloc_free/101-strtow.c
@@ -0,0 +1,68 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* strtow - Splits a string into words
+* @str: The string to split
+* Return: Pointer to an array of strings (words), or NULL on failure
+*/
+char **strtow(char *str)
+{
+    unsigned int i = 0, j, w = 0, n = 0, start, end, length;
+    char **words;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	while (str[i])
+	{
+		while (str[i] == ' ')
+			i++;
+		if (str[i])
+		{
+			n++;
+			while (str[i] != ' ' && str[i] != '\0')
+				i++;
+		}
+	}
+
+	words = malloc(sizeof(char *) * (n + 1));
+	if (words == NULL)
+		return (NULL);
+	words[n] = NULL;
+
+	i = 0;
+    w = 0;
+	while (str[i] && w < n)
+    {
+        while (str[i] == ' ')
+            i++;
+        start = i;
+        while (str[i] != ' ' && str[i] != '\0')
+            i++;
+        end = i;
+
+        length = end - start;
+        words[w] = malloc(sizeof(char) * (length + 1));
+        if (words[w] == NULL)
+        {
+            while (w > 0)
+            {
+                w--;
+                free(words[w]);
+            }
+            free(words);
+            return NULL;
+        }
+		
+		for (i = start, j = 0; j < length; j++, i++)
+            words[w][j] = str[i];
+        words[w][length] = '\0';
+        w++;
+        i = end;
+    }
+
+	words[n] = NULL;
+	return (words);
+}
