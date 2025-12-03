36fdf94fd8ddb4ac2125d33bca60eeed3ce05de8
Gigi-Corlay <11489@holbertonstudents.com>
2025-12-03 10:46:15 +0100

    Write a function that adds a new node at the beginning of a dlistint_t list

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -6,25 +6,31 @@
 /**
  * add_dnodeint - Adds a new node at the beginning of a doubly linked list
  * @head: Double pointer to the head of the list
- * @n: Value to be stored in the new node
+ * @n: Value to store in the new node
  *
- * Description: Allocates memory for a new node, initializes its value,
- *              inserts it at the beginning of the list, and updates
- *              the previous head node if the list is not empty.
+ * Description:
+ * Allocates memory for a new doubly linked list node, initializes its fields,
+ * and inserts it at the beginning of the list. If the list is not empty,
+ * updates the previous head node to correctly reference the new node.
  *
- * Return: Address of the new node, or NULL if memory allocation fails
+ * Return: Address of the new node, or NULL if memory allocation fails.
  */
- dlistint_t *add_dnodeint(dlistint_t **head, const int n)
- {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
 
-	if (*head)
+	if (*head != NULL)
 		(*head)->prev = new_node;
+
 	*head = new_node;
+
 	return (new_node);
- }
+}
