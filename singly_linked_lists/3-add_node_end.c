cf68e66c23f26f98771a2fd50a80739ab736b76b
Gigi-Corlay <11489@holbertonstudents.com>
2025-12-03 15:20:34 +0100

    Rewrite the code without strlen

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -3,42 +3,50 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a list_t list
- * @head: pointer to the head of the list
- * @str: string to duplicate and store in the new node
- *
- * Return: address of the new node, or NULL if it failed
- */
+* add_node_end - adds a new node at the end of a list_t list
+* @head: pointer to the head of the list
+* @str: string to duplicate and store in the new node
+*
+* Return: address of the new node, or NULL if it failed
+*/
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *last;
-	list_t *new = malloc(sizeof(list_t));
+	list_t *new;
+	list_t *temp;
+	int len = 0;
 
-	if (!new)
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 		return (NULL);
 
 	new->str = strdup(str);
-
-	if (!new->str)
+	if (new->str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	new->len = strlen(str);
+
+	new->len = len;
 	new->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new;
+		return (new);
 	}
-	else
-	{
-		last = *head;
-		while (last->next != NULL)
-		{
-			last = last->next;
-		}
-		last->next = new;
-	}
+
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new;
+
 	return (new);
 }
