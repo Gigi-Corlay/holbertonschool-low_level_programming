62ded4275263608781efa5b2db6cddb966c601ed
Gigi-Corlay <11489@holbertonstudents.com>
2025-12-03 20:54:26 +0100

    fixe: coding betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -4,16 +4,17 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a doubly linked list
- * @h: Constant pointer to the head of the list
- *
- * Description: Iterates through a doubly linked list, printing each node's
- *              value, and counts the total number of nodes.
- *
- * Return: The number of nodes printed
- */
- size_t print_dlistint(const dlistint_t *h)
- {
+* print_dlistint - Prints all the elements of a doubly linked list
+* @h: Constant pointer to the head of the list
+*
+* Description: Iterates through a doubly linked list, printing each node's
+*              value, and counts the total number of nodes.
+*
+* Return: The number of nodes printed
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
 	size_t count = 0;
 
 	while (h)
@@ -23,4 +24,4 @@
 		count++;
 	}
 	return (count);
- }
+}
